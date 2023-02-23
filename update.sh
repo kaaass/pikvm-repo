1032a6f651443e5b81ca061cd2c122626c23aef3
KAAAsS <admin@kaaass.net>
2023-02-24 00:13:21 +0800

    feat: remove symlink

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+rm -rf generic-arm
 cp -a ../packages/repos/* .
 for f in $(find -type l); do 
     cp --remove-destination `readlink -f "$f"` "$f"
 done
+cp -a generic generic-arm
 git add .
 git commit -m "sync: latest build"
 git push
