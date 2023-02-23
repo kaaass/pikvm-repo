1f687cca53ff411464bff5808a3b160dc2c28264
KAAAsS <admin@kaaass.net>
2023-02-23 22:18:14 +0800

    feat: remove symlink while updating

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 cp -a ../packages/repos/* .
+for f in $(find -type l); do 
+    cp --remove-destination `readlink -f "$f"` "$f"
+done
 git add .
 git commit -m "sync: latest build"
 git push
