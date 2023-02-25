869acf20b93175e4128087d6513e3c9632387d00
KAAAsS <admin@kaaass.net>
2023-02-25 12:17:12 +0800

    Revert "Revert "feat: use BOARD-ARCH as actual path""
    This reverts commit f28b37408549c36298f9b46dc44c9f48201e2383.

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+rm -f generic
+mv generic-arm generic
 cp -a ../packages/repos/* .
 for f in $(find -type l); do 
     cp --remove-destination `readlink -f "$f"` "$f"
 done
+mv generic generic-arm
+ln -s generic-arm generic
 git add .
 git commit -m "sync: latest build"
 git push
